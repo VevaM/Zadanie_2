5a87a7c2e029a86f2c278448b95629bcf974d52c
VevaM <veronika1stef@gmail.com>
2023-12-03 15:23:05 +0100

    odrazit sa tu 1

--- main.cpp
+++ main.cpp
@@ -633,7 +633,7 @@ void sendM(bool * rec, bool * connection, bool *keepalive, bool *recievFr , bool
                 role = "klient";
                 serverAdd = clientAdd;
                 cout << "rola " << role;
-                *changeRole = false;
+                *changeRole = true;
                 //changedRoles= false;
                // this_thread::sleep_for(1000ms);
                 changeRoleTo("klient",rec,connection,keepalive,recievFr,changeRole,correctData);
@@ -709,7 +709,7 @@ void receiveM(bool * rec, bool * connection, bool *keepalive ,bool *recievFr , b
                 else if(toBinary((int)header1.type) == "00000010" && *connection && *changeRole){
                     cout << "mozeme zo spravit";
                    // role = "server";
-                    *changeRole = false;
+                    *changeRole = true;
                     //changedRoles = false;
                     clientAdd = serverAdd;
                     //*connection = false;
